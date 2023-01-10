be6270188bcf056cc01a121b7472170bf1cf9b59
root <vigneshkoduru321@gmail.com>
2023-01-10 06:57:52 +0000

    first commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -17,7 +17,7 @@ import SliderCard from '../SliderCard'
 import SliderCardSm from '../SliderCardSm'
 
 class Home extends Component {
-  state = {dataState: 'initial', Data: []}
+  state = {dataState: 'initial'}
 
   componentDidMount() {
     this.getTopRatedBooksData()
@@ -48,7 +48,7 @@ class Home extends Component {
   }
 
   renderLoading = () => (
-    <div testid="loader" className="loader-con-home">
+    <div testid="loader" className="data-con">
       <div className="products-loader-container">
         <Loader type="ThreeDots" color="#0b69ff" height="50" width="50" />
       </div>
@@ -63,39 +63,16 @@ class Home extends Component {
       slidesToScroll: 1,
     }
     return (
-      <ul className="slider-container">
-        <li>
-          <Slider className="slider" {...settings}>
+      <div className="slider-container">
+        <ul>
+          <Slider {...settings}>
             {Data.map(each => (
-              <SliderCard details={each} key={each.id} />
+              <li>
+                <SliderCard details={each} key={each.id} />
+              </li>
             ))}
           </Slider>
-        </li>
-      </ul>
-    )
-  }
-
-  renderSuccessSmall = () => {
-    const settingsSm = {
-      dots: false,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-    }
-    const {Data} = this.state
-    const settings = {
-      dots: false,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-    }
-    return (
-      <div className="slider-container-sm">
-        <li>
-          <Slider className="slider-sm" {...settingsSm}>
-            {Data.map(each => (
-              <SliderCardSm details={each} key={each.id} />
-            ))}
-          </Slider>
-        </li>
+        </ul>
       </div>
     )
   }
@@ -128,60 +105,30 @@ class Home extends Component {
     }
   }
 
-  renderItemSm() {
-    const {dataState} = this.state
-    switch (dataState) {
-      case 'loading':
-        return this.renderLoading()
-      case 'success':
-        return this.renderSuccessSmall()
-      case 'failure':
-        return this.renderFailure()
-      default:
-        return null
-    }
-  }
-
   render() {
     return (
-      <ul className="main-home-container">
-        <div className="header-container-lg">
-          <Header />
-        </div>
-        <div className="header-container-sm">
-          <HeaderSm />
-        </div>
-        <div className="scroll-con">
-          <li className="find-content-conatiner">
-            <h1 className="find-heading">Find Your Next Favorite Books?</h1>
-            <p className="find-para">
-              You are in the right place. Tell us what titles or genres you have
-              enjoyed in the past, and we will give you surprisingly insightful
-              recommendations.
-            </p>
-            <Link style={{textDecoration: 'none'}} to="/shelf">
-              <button className="find-books-button">Find Books</button>
-            </Link>
-          </li>
-          <div className="top-rated-container">
-            <h1 className="top-heading">Top Rated Books</h1>
-            <Link style={{textDecoration: 'none'}} to="/shelf">
-              <button className="find-books-button-lg">Find Books</button>
+      <ul className="home-con">
+        <Header />
+        <li className="banner-con">
+          <h1 className="heading">Find Your Next Favorite Books?</h1>
+          <p className="paragraph">
+            You are in the right place. Tell us what titles or genres you have
+            enjoyed in the past, and we will give you surprisingly insightful
+            recommendations.
+          </p>
+        </li>
+        <li className="div-top">
+          <div className="top-head-button">
+            <h1 className="head">Top Rated Books</h1>
+            <Link to="/shelf">
+              <button className="find-books">Find Books</button>
             </Link>
           </div>
-          <div className="slider-main-container">
-            <div className="slider-container-main">{this.renderItem()}</div>
-            <div className="slider-container-main-sm">
-              {this.renderItemSm()}
-            </div>
-          </div>
-          <div className="footer-con">
-            <Footer />
-          </div>
-        </div>
+          <div className="data-con">{this.renderItem()}</div>
+        </li>
+        <Footer />
       </ul>
     )
   }
 }
-
 export default Home
